ae67daf346f89a985813ca81d13c85b07f878d1d
Bernd Hahnebach <bernd@bimstatik.org>
2017-08-28 17:53:07 +0200

    FEM: prefs, use new solver for ccx as standard, we might not use this for PR, but for testing it should be set as default

--- src/Mod/Fem/PyGui/_CommandFemAnalysis.py
+++ src/Mod/Fem/PyGui/_CommandFemAnalysis.py
@@ -50,7 +50,7 @@ class _CommandFemAnalysis(FemCommands):
         FreeCADGui.doCommand("ObjectsFem.makeAnalysis('Analysis')")
         FreeCADGui.doCommand("FemGui.setActiveAnalysis(FreeCAD.ActiveDocument.ActiveObject)")
         ccx_prefs = FreeCAD.ParamGet("User parameter:BaseApp/Preferences/Mod/Fem/Ccx")
-        use_old_solver_frame_work = ccx_prefs.GetBool("useOldSolverFrameWork", True)
+        use_old_solver_frame_work = ccx_prefs.GetBool("useOldSolverFrameWork", False)
         if use_old_solver_frame_work:
             FreeCADGui.doCommand("ObjectsFem.makeSolverCalculixOld()")
         else:
