a86582ad4008349a4c08c7c022c2fb745fcaf2b4
Bernd Hahnebach <bernd@bimstatik.org>
2017-09-29 08:16:37 +0200

    FEM: objects fem, sort defs

--- src/Mod/Fem/ObjectsFem.py
+++ src/Mod/Fem/ObjectsFem.py
@@ -72,12 +72,6 @@ def makeConstraintFixed(doc, name="ConstraintFixed"):
     return obj
 
 
-def makeConstraintFluidBoundary(doc, name="ConstraintFluidBoundary"):
-    '''makeConstraintFluidBoundary(name): makes a Fem ConstraintForce object'''
-    obj = doc.addObject("Fem::ConstraintForce", name)
-    return obj
-
-
 def makeConstraintFlowVelocity(doc, name="ConstraintFlowVelocity"):
     obj = doc.addObject("Fem::ConstraintPython", name)
     import PyObjects._FemConstraintFlowVelocity
@@ -85,6 +79,12 @@ def makeConstraintFlowVelocity(doc, name="ConstraintFlowVelocity"):
     if FreeCAD.GuiUp:
         import PyGui._ViewProviderFemConstraintFlowVelocity
         PyGui._ViewProviderFemConstraintFlowVelocity.ViewProxy(obj.ViewObject)
+    return obj
+
+
+def makeConstraintFluidBoundary(doc, name="ConstraintFluidBoundary"):
+    '''makeConstraintFluidBoundary(name): makes a Fem ConstraintForce object'''
+    obj = doc.addObject("Fem::ConstraintForce", name)
     return obj
 
 
