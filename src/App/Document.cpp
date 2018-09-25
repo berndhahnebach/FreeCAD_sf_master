b5e7edb9b968a15578d75e0ca18598dd5893aa78
Ian Rees <ian.rees@gmail.com>
2018-09-25 17:44:29 +1200

    Minor rebase fixes + Py3 work

--- src/App/Document.cpp
+++ src/App/Document.cpp
@@ -2065,6 +2065,7 @@ Document::getDependencyList(const std::vector<App::DocumentObject*>& objs) const
         ary.push_back(VertexMap[*it]);
     return ary;
 }
+#endif // #ifdef USE_OLD_DAG
 
 App::MaterialDatabase &Document::getMaterialDatabase()
 {
@@ -2077,7 +2078,6 @@ App::MaterialDatabase &Document::getMaterialDatabase()
 
     return *d->materialDatabase;
 }
-#endif
 
 void Document::_rebuildDependencyList(void)
 {
