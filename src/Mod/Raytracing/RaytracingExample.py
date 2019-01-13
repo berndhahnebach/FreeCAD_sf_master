7710d1129bd475ecede94544d6b62c0831559971
Bernd Hahnebach <bernd@bimstatik.org>
2019-01-13 07:05:21 +0100

    Raytracing: pep8 code formating, get rid of multi line import, flake E401

--- src/Mod/Raytracing/RaytracingExample.py
+++ src/Mod/Raytracing/RaytracingExample.py
@@ -1,7 +1,9 @@
 # example how to use the scripting API of the drawing module
 # 
 # first of all you need the Part and the Drawing module:
-import FreeCAD, Part, Drawing
+import FreeCAD
+import Part
+import Drawing
 
 # create a small sample part
 Part.show(Part.makeBox(100,100,100).cut(Part.makeCylinder(80,100)).cut(Part.makeBox(90,40,100)).cut(Part.makeBox(20,85,100)))
