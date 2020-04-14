0f4fe6c8f2548fbe06165658e4b734534320a16c
Sudhanshu Dubey <sudhanshu.thethunder@gmail.com>
2020-04-14 20:21:38 +0530

    Fem: Model setup completed in torque example

--- src/Mod/Fem/femexamples/torque_example.py
+++ src/Mod/Fem/femexamples/torque_example.py
@@ -23,6 +23,9 @@
 
 # to run the example use:
 """
+doc = App.newDocument("Torque_example")
+from femexamples import torque_example as torque
+torque.setup_cylinderbase(doc)
 """
 
 import FreeCAD
@@ -36,4 +39,68 @@ mesh_name = "Mesh"  # needs to be Mesh to work with unit tests
 def init_doc(doc=None):
     if doc is None:
         doc = FreeCAD.newDocument()
+    return doc
+
+
+def setup_cylinderbase(doc=None, solvertype="ccxtools"):
+    # setup cylinder base model
+
+    if doc is None:
+        doc = init_doc()
+
+    # geometry object
+    # name is important because the other method in this module use obj name
+    cylinder1 = doc.addObject("Part::Cylinder", "Cylinder1")
+    cylinder1.Height = "50 mm"
+    cylinder1.Radius = "5 mm"
+    cylinder2 = doc.addObject("Part::Cylinder", "Cylinder2")
+    cylinder2.Height = "50 mm"
+    cylinder2.Radius = "4 mm"
+    geom_obj = doc.addObject("Part::Cut", "Cut")
+    geom_obj.Base = cylinder1
+    geom_obj.Tool = cylinder2
+    doc.recompute()
+
+    if FreeCAD.GuiUp:
+        geom_obj.ViewObject.Document.activeView().viewAxonometric()
+        geom_obj.ViewObject.Document.activeView().fitAll()
+
+    # analysis
+    analysis = ObjectsFem.makeAnalysis(doc, "Analysis")
+
+    # solver
+    if solvertype == "calculix":
+        solver_object = analysis.addObject(
+            ObjectsFem.makeSolverCalculix(doc, "SolverCalculiX")
+        )[0]
+    elif solvertype == "ccxtools":
+        solver_object = analysis.addObject(
+            ObjectsFem.makeSolverCalculixCcxTools(doc, "CalculiXccxTools")
+        )[0]
+        solver_object.WorkingDir = u""
+    elif solvertype == "elmer":
+        analysis.addObject(ObjectsFem.makeSolverElmer(doc, "SolverElmer"))
+    elif solvertype == "z88":
+        analysis.addObject(ObjectsFem.makeSolverZ88(doc, "SolverZ88"))
+    if solvertype == "calculix" or solvertype == "ccxtools":
+        solver_object.SplitInputWriter = False
+        solver_object.AnalysisType = "static"
+        solver_object.GeometricalNonlinearity = "linear"
+        solver_object.ThermoMechSteadyState = False
+        solver_object.MatrixSolverType = "default"
+        solver_object.IterationsControlParameterTimeUse = False
+
+    # material
+    material_object = analysis.addObject(
+        ObjectsFem.makeMaterialSolid(doc, "FemMaterial")
+    )[0]
+    mat = material_object.Material
+    mat["Name"] = "CalculiX-Steel"
+    mat["YoungsModulus"] = "210000 MPa"
+    mat["PoissonRatio"] = "0.30"
+    mat["Density"] = "7900 kg/m^3"
+    mat["ThermalExpansionCoefficient"] = "0.012 mm/m/K"
+    material_object.Material = mat
+
+    doc.recompute()
     return doc
