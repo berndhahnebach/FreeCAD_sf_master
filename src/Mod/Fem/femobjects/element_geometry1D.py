fa7128799c6e1a82ed77aa75da70a04ff2f6930c
ebrahimraeyat <ebe79442114@yahoo.com>
2020-04-26 21:26:42 +0200

    FEM: element geometry 1D, add some properties

--- src/Mod/Fem/femobjects/element_geometry1D.py
+++ src/Mod/Fem/femobjects/element_geometry1D.py
@@ -29,6 +29,8 @@ __url__ = "https://www.freecadweb.org"
 #  \ingroup FEM
 #  \brief element geometry 1D object
 
+import math
+
 from . import base_fempythonobject
 
 
@@ -92,5 +94,62 @@ class ElementGeometry1D(base_fempythonobject.BaseFemPythonObject):
             "List of beam section shapes"
         )
 
+        obj.addProperty(
+            "App::PropertyFloat",
+            "Area",
+            "Properties",
+            "section Area",
+        )
+
+        obj.addProperty(
+            "App::PropertyFloat",
+            "Izz",
+            "Properties",
+            "section moment of inertia about Z axis")
+
+        obj.addProperty(
+            "App::PropertyFloat",
+            "Iyy",
+            "Properties",
+            "section moment of inertia about Y axis")
+
+        obj.addProperty(
+            "App::PropertyFloat",
+            "J",
+            "Properties",
+            "section torsion constant")
+
         obj.SectionType = ElementGeometry1D.known_beam_types
         obj.SectionType = "Rectangular"
+
+    def execute(self, obj):
+        if obj.SectionType == "Rectangular":
+            w = obj.RectWidth.Value
+            h = obj.RectHeight.Value
+            obj.Area = w * h
+            obj.Izz = w * h ** 3 / 12
+            obj.Iyy = h * w ** 3 / 12
+            if w > h:
+                a = w / 2
+                b = h / 2
+            else:
+                a = h / 2
+                b = w / 2
+            obj.J = a * b ** 3 * (16 / 3 - 3.36 * b / a * (1 - b ** 4 / (12 * a ** 4)))
+
+        elif obj.SectionType == "Circular":
+            d = obj.CircDiameter.Value
+            obj.Area = math.pi * d ** 2 / 4
+            obj.Iyy = math.pi * (d / 2) ** 4 / 4
+            obj.Izz = obj.Iyy
+            obj.J = obj.Iyy * 2
+
+        elif obj.SectionType == "Pipe":
+            do = obj.PipeDiameter.Value
+            t = obj.PipeThickness.Value
+            di = do - 2 * t
+            obj.Area = math.pi * (do ** 2 - di ** 2) / 4
+            obj.Iyy = math.pi * (d / 2) ** 3 * obj.PipeThickness.Value
+            obj.Izz = obj.Iyy
+
+        return
