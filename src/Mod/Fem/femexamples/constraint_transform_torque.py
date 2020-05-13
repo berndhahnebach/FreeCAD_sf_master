26f03a11c988fe48fe2a85ba904425f9c490274d
Bernd Hahnebach <bernd@bimstatik.org>
2020-05-13 08:19:56 +0200

    FEM: torque example, fix it

--- src/Mod/Fem/femexamples/constraint_transform_torque.py
+++ src/Mod/Fem/femexamples/constraint_transform_torque.py
@@ -1,5 +1,6 @@
 # ***************************************************************************
 # *   Copyright (c) 2020 Sudhanshu Dubey <sudhanshu.thethunder@gmail.com>   *
+# *   Copyright (c) 2020 Bernd Hahnebach <bernd@bimstatik.org>              *
 # *                                                                         *
 # *   This file is part of the FreeCAD CAx development system.              *
 # *                                                                         *
@@ -23,15 +24,20 @@
 
 # to run the example use:
 """
-doc = App.newDocument("Torque_example")
 from femexamples import constraint_transform_torque as torque
-torque.setup(doc)
+torque.setup()
+
 """
+
+# constraint transform with a constraint force
+# https://forum.freecadweb.org/viewtopic.php?t=19037
+# https://forum.freecadweb.org/viewtopic.php?t=18970
 
 import FreeCAD
 
 import Fem
 import ObjectsFem
+from Part import makeLine
 
 mesh_name = "Mesh"  # needs to be Mesh to work with unit tests
 
@@ -42,8 +48,7 @@ def init_doc(doc=None):
     return doc
 
 
-def setup_cylinderbase(doc=None, solvertype="ccxtools"):
-    # setup cylinder base model
+def setup(doc=None, solvertype="ccxtools"):
 
     if doc is None:
         doc = init_doc()
@@ -61,9 +66,18 @@ def setup_cylinderbase(doc=None, solvertype="ccxtools"):
     geom_obj.Tool = cylinder2
     doc.recompute()
 
+    # line for load direction
+    sh_load_line = makeLine(FreeCAD.Vector(0, 0, 0), FreeCAD.Vector(0, 10, 0))
+    load_line = doc.addObject("Part::Feature", "Load_direction_line")
+    load_line.Shape = sh_load_line
+
     if FreeCAD.GuiUp:
+        load_line.ViewObject.LineWidth = 5.0
+        load_line.ViewObject.LineColor = (1.0, 0.0, 0.0)
         geom_obj.ViewObject.Document.activeView().viewAxonometric()
         geom_obj.ViewObject.Document.activeView().fitAll()
+
+    doc.recompute()
 
     # analysis
     analysis = ObjectsFem.makeAnalysis(doc, "Analysis")
@@ -98,19 +112,35 @@ def setup_cylinderbase(doc=None, solvertype="ccxtools"):
     mat["Name"] = "CalculiX-Steel"
     mat["YoungsModulus"] = "210000 MPa"
     mat["PoissonRatio"] = "0.30"
-    mat["Density"] = "7900 kg/m^3"
-    mat["ThermalExpansionCoefficient"] = "0.012 mm/m/K"
     material_object.Material = mat
 
-    # fixed_constraint
+    # constraint fixed
     fixed_constraint = analysis.addObject(
         ObjectsFem.makeConstraintFixed(doc, name="ConstraintFixed")
     )[0]
     fixed_constraint.References = [(geom_obj, "Face3")]
 
+    # constraint force
+    force_constraint = doc.Analysis.addObject(
+        ObjectsFem.makeConstraintForce(doc, name="ConstraintForce")
+    )[0]
+    force_constraint.References = [(geom_obj, "Face1")]
+    force_constraint.Force = 100000.0
+    force_constraint.Direction = (load_line, ["Edge1"])
+    force_constraint.Reversed = True
+
+    # constraint transform
+    transform_constraint = doc.Analysis.addObject(
+        ObjectsFem.makeConstraintTransform(doc, name="ConstraintTransform")
+    )[0]
+    transform_constraint.References = [(geom_obj, "Face1")]
+    transform_constraint.TransformType = "Cylindrical"
+    transform_constraint.X_rot = 0.0
+    transform_constraint.Y_rot = 0.0
+    transform_constraint.Z_rot = 0.0
+
     # mesh
     from .meshes.mesh_transform_torque import create_nodes, create_elements
-
     fem_mesh = Fem.FemMesh()
     control = create_nodes(fem_mesh)
     if not control:
@@ -118,46 +148,10 @@ def setup_cylinderbase(doc=None, solvertype="ccxtools"):
     control = create_elements(fem_mesh)
     if not control:
         FreeCAD.Console.PrintError("Error on creating elements.\n")
-    femmesh_obj = analysis.addObject(ObjectsFem.makeMeshGmsh(doc, mesh_name))[0]
+    femmesh_obj = analysis.addObject(
+        doc.addObject("Fem::FemMeshObject", mesh_name)
+    )[0]
     femmesh_obj.FemMesh = fem_mesh
-    femmesh_obj.SecondOrderLinear = False
-
-    doc.recompute()
-    return doc
-
-
-def setup_forceonsurface(doc=None, solvertype="ccxtools"):
-    # setup force on the surface of cylinder
-
-    doc = setup_cylinderbase(doc, solvertype)
-
-    # force_constraint
-    force_constraint = doc.Analysis.addObject(
-        ObjectsFem.makeConstraintForce(doc, name="ConstraintForce")
-    )[0]
-    force_constraint.References = [(doc.Cut, "Face1")]
-    force_constraint.Force = 100000.0
-    force_constraint.Direction = None
-    force_constraint.Reversed = True
-
-    doc.recompute()
-    return doc
-
-
-def setup(doc=None, solvertype="ccxtools"):
-    # Transform the coordinates of the cylinder
-
-    doc = setup_forceonsurface(doc, solvertype)
-
-    # transform_constraint
-    transform_constraint = doc.Analysis.addObject(
-        ObjectsFem.makeConstraintTransform(doc, name="ConstraintTransform")
-    )[0]
-    transform_constraint.References = [(doc.Cut, "Face1")]
-    transform_constraint.TransformType = "Cylindrical"
-    transform_constraint.X_rot = 0.000000
-    transform_constraint.Y_rot = 0.000000
-    transform_constraint.Z_rot = 0.000000
 
     doc.recompute()
     return doc
