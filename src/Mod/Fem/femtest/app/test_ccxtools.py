dede2b6277481e01b762457b1b9bc6b7500a745a
Sudhanshu Dubey <sudhanshu.thethunder@gmail.com>
2020-05-23 03:47:57 +0530

    FEM: Unit Test, add transform torque writing

--- src/Mod/Fem/femtest/app/test_ccxtools.py
+++ src/Mod/Fem/femtest/app/test_ccxtools.py
@@ -232,6 +232,27 @@ class TestCcxTools(unittest.TestCase):
         )
 
     # ********************************************************************************************
+    def test_static_constraint_transform_torque(
+        self
+    ):
+        # set up
+        from femexamples.constraint_transform_torque import setup
+        setup(self.document, "ccxtools")
+        test_name = "constraint transform torque"
+        base_name = "constraint_transform_torque"
+        analysis_dir = testtools.get_unit_test_tmp_dir(
+            self.temp_dir,
+            "FEM_ccx_constraint_transform_torque",
+        )
+
+        # test input file writing
+        self.input_file_writing_test(
+            test_name=test_name,
+            base_name=base_name,
+            analysis_dir=analysis_dir,
+        )
+
+    # ********************************************************************************************
     def test_static_material_multiple(
         self
     ):
